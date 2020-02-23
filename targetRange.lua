176366808ce9f20ccefe3585924c74c9dd5c28c9
rparliment <38598967+rparliment@users.noreply.github.com>
2020-02-23 13:58:05 -0500

    Update targetRange.lua

--- targetRange.lua
+++ targetRange.lua
@@ -72,36 +72,26 @@ local targetRangeCheck = CreateFrame("FRAME")
 
 local function rangeCheck(self)
     if IsSpellInRange("Fire Blast","Target") ~= nil then
-        if (CheckInteractDistance("Target", 3) == true and trPrev ~= 1) then
+        if (CheckInteractDistance("Target", 3) == true) then
             targetRangeFrame.Text:SetText("|cFFFF0000< 7 yd|r")
-            trPrev = 1
-        elseif (CheckInteractDistance("Target", 2) == true and CheckInteractDistance("Target", 3) ~= true and trPrev ~= 2) then
+        elseif (CheckInteractDistance("Target", 2) == true and CheckInteractDistance("Target", 3) ~= true) then
             targetRangeFrame.Text:SetText("|cFFFF00007–8 yd|r")
-            trPrev = 2
-        elseif (IsSpellInRange("Fire Blast","Target") == 1 and CheckInteractDistance("Target", 2) ~= true and trPrev ~= 3) then
+        elseif (IsSpellInRange("Fire Blast","Target") == 1 and CheckInteractDistance("Target", 2) ~= true) then
             targetRangeFrame.Text:SetText("|cFFFF00008–20 yd|r")
-            trPrev = 3
-        elseif (CheckInteractDistance("Target", 4) == true and IsSpellInRange("Fire Blast","Target") ~= 1 and trPrev ~= 4) then
-            targetRangeFrame.Text:SetText("20–28 yd")
-            trPrev = 4           
-        elseif (IsSpellInRange("Polymorph","Target") == 1 and CheckInteractDistance("Target", 4) ~= true and trPrev ~= 5) then
+        elseif (CheckInteractDistance("Target", 4) == true and IsSpellInRange("Fire Blast","Target") ~= 1) then
+            targetRangeFrame.Text:SetText("20–28 yd")         
+        elseif (IsSpellInRange("Polymorph","Target") == 1 and CheckInteractDistance("Target", 4) ~= true) then
             targetRangeFrame.Text:SetText("|cFFFFA50028–30 yd|r")
-            trPrev = 5
-        elseif (IsSpellInRange("Fireball","Target") == 1 and IsSpellInRange("Polymorph","Target") ~= 1 and trPrev ~= 6) then
+        elseif (IsSpellInRange("Fireball","Target") == 1 and IsSpellInRange("Polymorph","Target") ~= 1) then
             targetRangeFrame.Text:SetText("|cFF00FF0030–35 yd|r")
-            trPrev = 6
-        elseif (IsSpellInRange("Frostbolt","Target") == 1 and IsSpellInRange("Fireball","Target") ~= 1 and trPrev ~= 7) then
+        elseif (IsSpellInRange("Frostbolt","Target") == 1 and IsSpellInRange("Fireball","Target") ~= 1) then
             targetRangeFrame.Text:SetText("|cFF00FF0035-36 yd|r")
-            trPrev = 7
-        elseif (IsSpellInRange("Detect Magic","Target") == 1 and IsSpellInRange("Frostbolt","Target") ~= 1 and trPrev ~= 8) then
+        elseif (IsSpellInRange("Detect Magic","Target") == 1 and IsSpellInRange("Frostbolt","Target") ~= 1) then
             targetRangeFrame.Text:SetText("|cFFFF000036–40 yd|r")
-            trPrev = 8
         elseif (IsSpellInRange("Detect Magic","Target") ~= 1 and trPrev ~=9) then
             targetRangeFrame.Text:SetText("|cFFFF0000> 40 yd|r")
-            trPrev = 9
         end
     else
-        trPrev = 0
         if rangeLock == 0 then
             targetRangeFrame.Text:SetText("targetRange")
         else
